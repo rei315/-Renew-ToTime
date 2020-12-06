c449757dae4a54c83ec74ed2319af33b9ce04b80
kimminguk <alsrnr315@gmail.com>
2020-12-06 22:48:50 +0900

    Add AddMarkController

--- ToTime/Controller/AddMarkController.swift
+++ ToTime/Controller/AddMarkController.swift
@@ -1,19 +1,23 @@
 //
-//  ViewController.swift
-//  ToTime
+//  AddMarkController.swift
+//  ToTimer
 //
 //  Created by 김민국 on 2020/12/06.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddMarkController: UIViewController {
 
+    // MARK: - Property
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
-
 }
-
