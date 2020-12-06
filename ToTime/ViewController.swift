399f42b2b3f33bbc5c00949d08d094c6923da07d
kimminguk <alsrnr315@gmail.com>
2020-12-06 20:03:21 +0900

    Initial Commit

---
+++ ToTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToTime
+//
+//  Created by 김민국 on 2020/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
