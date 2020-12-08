956f59d120b37035840ef97a31ea2c0192b07811
kimminguk <alsrnr315@gmail.com>
2020-12-09 02:08:54 +0900

    Modify ResourceManager to handle error

--- ToTime/Helper/ResourceManager.swift
+++ ToTime/Helper/ResourceManager.swift
@@ -14,16 +14,22 @@ struct ResourceManager {
     func fetchMarkIcons() -> Observable<UIImage>{
         Observable.create { observer in
             let fileManager = FileManager.default
-            let imagePath = Bundle.main.resourcePath! + "/MarkIcons"
-            
-            let imageNames = try! fileManager.contentsOfDirectory(atPath: imagePath)
-            for name in imageNames {
-                if let imagePath = Bundle.main.path(forResource: name, ofType: nil) {
-                    if let image = UIImage(contentsOfFile: imagePath) {
-                        observer.onNext(image)
+            let imagePath = Bundle.main.resourcePath! + "/MarkIcon"
+            do {
+                let imageNames = try fileManager.contentsOfDirectory(atPath: imagePath)
+                for name in imageNames {
+                    if let imagePath = Bundle.main.path(forResource: name, ofType: nil) {
+                        if let image = UIImage(contentsOfFile: imagePath) {
+                            print(image.description)
+                            observer.onNext(image)
+                        }
                     }
                 }
+            } catch {
+                print(error)
+                observer.onError(error)
             }
+            
             observer.onCompleted()            
             
             return Disposables.create()
