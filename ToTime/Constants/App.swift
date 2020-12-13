c5dcd40e013660b36e3557d4e20f2cd8b8f7c4c4
kimminguk <alsrnr315@gmail.com>
2020-12-13 21:22:11 +0900

    App Localization Done - Korean, English Not Yet - Japanese

--- ToTime/Constants/App.swift
+++ ToTime/Constants/App.swift
@@ -14,7 +14,44 @@ extension App {
 
 enum AppString {}
 extension AppString {
-    static let destination = "목적지"
+    static let Destination = "Destination"
+    static let NotificationID = "reach_notification_id"
+    static let LocationID = "reach_location_id"
+    static let NotificationTitle = "ToTimeNotificationTitle"
+    static let NotificationBody = "ToTimeNotificationBody"
+    static let NotificationPermissionTitle = "NotificationPermissionTitle"
+    static let NotificationPermissionMessage = "NotificationPermissionBody"
+    static let LocationDeniedTitle = "LocationDeniedTitle"
+    static let LocationDeniedMessage = "LocationDeniedBody"
+    static let NotificationErrorTitle = "NotificationErrorTitle"
+    static let NotificationErrorMessage = "NotificationErrorBody"
+    
+    static let Fifty = "50M"
+    static let Hundred = "100M"
+    static let Thousand = "1000M"
+    static let DistanceEdit = "QuickMapDistanceEdit"
+    
+    static let QuickMapStart = "QuickMapStart"
+    static let QuickMapEditValuePlaceHolder = "QuickMapEditValuePlaceHolder"
+    static let Enter = "Enter"
+    static let QuickMapNamePlaceHolder = "QuickMapNamePlaceHolder"
+    static let QuickMapAddFavorite = "QuickMapAddFavorite"
+    static let QuickMapDeleteFavorite = "QuickMapDeleteFavorite"
+    static let QuickMapModifyFavorite = "QuickMapModifyFavorite"
+    
+    static let AddressPlaceHolder = "AddressPlaceHolder"
+    static let HomeTitle = "HomeTitle"
+    static let FavoriteTitle = "FavoriteTitle"
+    
+    static let InputError = "InputError"
+    static let FavoriteAddressEmptyAlertMessage = "FavoriteAddressEmptyAlertMessage"
+    static let FavoriteNameEmptyAlertMessage = "FavoriteNameEmptyAlertMessage"
+    static let FavoriteImageEmptyAlertMessage = "FavoriteImageEmptyAlertMessage"
+    
+    static let CreatedComplete = "CreatedComplete"
+    static let UpdateComplete = "UpdateComplete"
+    static let FavoriteCreated = "FavoriteCreated"
+    static let FavoriteUpdated = "FavoriteUpdated"
 }
 
 enum Url {}
@@ -26,5 +63,5 @@ extension Url {
 
 enum NavigationTitle {}
 extension NavigationTitle {
-    static let DestinationAddress = "목적지 주소"
+    static let DestinationAddress = "SearchAddressTitle"
 }
